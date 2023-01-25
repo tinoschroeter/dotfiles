ecdb76db07927513a4a6e6ca1b428a487ab18468
Tino Schroeter <tino.schroter@eventim.de>
2023-01-25 17:44:59 +0100

    add: git command

--- playbook/files/cht.sh
+++ playbook/files/cht.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-languages="javascript\nnodejs\nexpress\nhtml\ncss\nsql\nsqlite\nreact\nbash\nsaltstack\nvim\n"
+languages="javascript\nnodejs\nexpress\nhtml\ncss\nsql\nsqlite\nreact\nbash\nsaltstack\nvim\ngit\n"
 core_utils="xargs\nfind\nmv\ncp\nsed\nawk\ncurl\ntr\nip"
 
-selected=`printf $languages$core_utils | fzf --no-border --height 5% --no-preview`
+selected=`printf $languages$core_utils | fzf --no-border --height 10% --no-preview`
 
 if [[ ${#selected} -eq 0 ]]; then
     exit 1 
