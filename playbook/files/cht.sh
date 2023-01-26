ea82e260ca7ab7bbf8e9f0b79795e67392129c43
Tino Schroeter <tino.schroter@eventim.de>
2023-01-27 00:06:50 +0100

    add: two functions

--- playbook/files/cht.sh
+++ playbook/files/cht.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
+util="dns_serial\ngen_password\n"
 languages="javascript\nnodejs\nexpress\nhtml\ncss\nsql\nsqlite\nreact\nbash\nsaltstack\nvim\ngit\n"
 core_utils="xargs\nfind\nmv\ncp\nsed\nawk\ncurl\ntr\nip"
 
-selected=`printf $languages$core_utils | fzf --no-border --height 10% --no-preview`
+selected=`printf $util$languages$core_utils| fzf --no-border --height 80% --no-preview --layout=reverse`
 
 if [[ ${#selected} -eq 0 ]]; then
     exit 1 
+fi
+
+if [[ "dns_serial" =~ ${selected} ]]; then
+  date_now=$(date +%Y%m%d)
+  echo "${date_now}01 ; Serial"
+  echo -n "${date_now}01" | xclip -selection c
+  exit 0
+fi
+
+if [[ "gen_password" =~ ${selected} ]]; then
+  pwd=`pwgen -s -1 -c 15`
+  echo -n ${pwd} | xclip -selection c
+  echo ${pwd}
+  exit 0
 fi
 
 if printf "$core_utils" | grep -qs "$selected"; then
