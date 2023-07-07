2031235f5e828d563281534e1702c23a4c18c64d
Tino Schroeter <tino.schroter@eventim.de>
2023-07-07 22:21:00 +0200

    override nvim-cmp width cmp-emoji

--- .config/nvim/lua/plugins/my-config.lua
+++ .config/nvim/lua/plugins/my-config.lua
@@ -1,5 +1,16 @@
 return {
 
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+    end,
+  },
+
   -- add telescope-fzf-native
   {
     "telescope.nvim",
