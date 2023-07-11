43cfd587efd059fb930cd140255c78af08e6adb9
Tino Schroeter <tino.schroter@eventim.de>
2023-07-11 23:06:27 +0200

    update: plugins

--- .config/nvim/lua/plugins/my-plugins.lua
+++ .config/nvim/lua/plugins/my-plugins.lua
@@ -1,11 +1,11 @@
 return {
+  { "L3MON4D3/LuaSnip", enabled = true },
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  { "hrsh7th/nvim-cmp", enabled = true },
   { "iamcco/markdown-preview.nvim", enabled = true },
-  { "vmware-archive/salt-vim", enabled = true },
-  { "simnalamburt/vim-mundo", enabled = true },
   { "mfussenegger/nvim-dap", enabled = false },
   { "rcarriga/nvim-dap-ui", enabled = false },
   { "rking/ag.vim", enabled = true },
-  { "hrsh7th/nvim-cmp", enabled = true },
-  { "L3MON4D3/LuaSnip", enabled = true },
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  { "simnalamburt/vim-mundo", enabled = true },
+  { "vmware-archive/salt-vim", enabled = true },
 }
